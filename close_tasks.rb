7bfaebaf19d944de6fea93d61f18c494c277b069
krivich ekaterina <krivich.ekaterina@gmail.com>
2012-07-27 21:02:01 +0400

    Update close_tasks.rb

--- close_tasks.rb
+++ close_tasks.rb
@@ -4,7 +4,11 @@ namespace :redmine do
 Close tickets with status 'resolved' and not changed for more than a week.
 END_DESC
     task :close => :environment do
-      
+     issues = Issue.all(:conditions => ["status_id=3 and updated_on <= :a_week", {:a_week => 1.week.ago.to_s(:db)}])   
+     issues.each do |issue|
+      # which status is closed??
+      #issuse.update_attribute(:status, 4)
+     end
     end
   end
 end
