91dd51f7e03194e3eb5be04c85b4624835006634
krivich ekaterina <krivich.ekaterina@gmail.com>
2012-08-01 15:06:13 +0400

    Update close_tasks.rb

--- close_tasks.rb
+++ close_tasks.rb
@@ -5,7 +5,7 @@ namespace :redmine do
     task :close => :environment do
      issues = Issue.all(:conditions => ["status_id=3 and updated_on <= :a_week", {:a_week => 1.week.ago.to_s(:db)}])   
      issues.each do |issue|
-      issue.update_attribute(:status, IssuseStatus.find(5))
+      issue.update_attribute(:status, IssueStatus.find(5))
      end
     end
   end
