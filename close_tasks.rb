ad1f92afa8d073da368f4bd25fa2f683b743a05e
krivich ekaterina <krivich.ekaterina@gmail.com>
2012-08-01 14:31:08 +0400

    set valid closed status to issues

--- close_tasks.rb
+++ close_tasks.rb
@@ -6,8 +6,7 @@ END_DESC
     task :close => :environment do
      issues = Issue.all(:conditions => ["status_id=3 and updated_on <= :a_week", {:a_week => 1.week.ago.to_s(:db)}])   
      issues.each do |issue|
-      # which status is closed??
-      #issuse.update_attribute(:status, 4)
+      issue.update_attribute(:status, 5)
      end
     end
   end
